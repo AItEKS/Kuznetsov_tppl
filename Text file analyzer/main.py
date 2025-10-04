c57c74f2959b10342ff18f6c3299c1b22b6d9a6a
AItEKS <129169524+AItEKS@users.noreply.github.com>
2025-10-04 09:16:26 +0800

    Create main.py

---
+++ Text file analyzer/main.py
@@ -0,0 +1,6 @@
+f = open("file.txt").readlines()
+
+print(f"Кол-во строк: {len(f)}")
+print(f"Кол-во символов: {sum([len(i) for i in f])}")
+print(f"Кол-во пустых строк: {sum([1 for i in f if i == '\n'])}")
+print("Словарь: ", {i: sum(line.count(i) for line in f) for i in set(''.join(f))})
