51b454839d267204157184822a4f1159dda521a2
AItEKS <129169524+AItEKS@users.noreply.github.com>
2025-10-04 09:36:57 +0800

    Update main.py

--- text_analyzer/main.py
+++ text_analyzer/main.py
@@ -1,5 +1,4 @@
-f_name = input("Путь до файа: ")
-f = open(f_name).readlines()
+f = open(input("Путь до файа: ")).readlines()
 
 wht_want = [int(i) for i in input("Что вы ходите узнать: \n1. Кол-во строк\n2. Кол-во символов\n3. Кол-во пустых строк\n4. Словарь\nВводить через пробел: ").split()]
 ans = [len(f), sum([len(i) for i in f]), sum([1 for i in f if i == '\n']), {i: sum(line.count(i) for line in f) for i in set(''.join(f))}]
